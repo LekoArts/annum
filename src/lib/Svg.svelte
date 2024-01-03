f246f6794efd39fac1f0de57ab0496fd213b7207
LekoArts <lekoarts@gmail.com>
2024-01-03 11:11:21 +0100

    icon component

---
+++ src/lib/Svg.svelte
@@ -0,0 +1,19 @@
+<script lang='ts'>
+	type SvgIconNames = 'movie' | 'tv' | 'home' | 'chevron-right'
+
+	export let id: SvgIconNames
+</script>
+
+<svg aria-hidden focusable='false' class='icon' {...$$restProps}>
+	<use href={`/icons.svg#${id}`} />
+</svg>
+
+<style lang='ts'>
+	.icon {
+		height: var(--space-xs-s);
+		width: var(--space-xs-s);
+		min-height: var(--space-xs-s);
+		min-width: var(--space-xs-s);
+		color: var(--icon-color, inherit);
+	}
+</style>
