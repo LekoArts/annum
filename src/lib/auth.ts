84612bf13a8db35072606bc9a4a1ff733332b093
Lennart <lekoarts@gmail.com>
2026-01-02 18:46:14 +0100

    feat: Add OAuthProxy (#18)

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,7 +2,7 @@ import { getRequestEvent } from '$app/server'
 import { PRIVATE_BETTER_AUTH_SECRET, PRIVATE_TRAKT_CLIENT_ID, PRIVATE_TRAKT_CLIENT_SECRET } from '$env/static/private'
 import { PUBLIC_BETTER_AUTH_URL } from '$env/static/public'
 import { betterAuth } from 'better-auth'
-import { customSession, genericOAuth } from 'better-auth/plugins'
+import { customSession, genericOAuth, oAuthProxy } from 'better-auth/plugins'
 import { sveltekitCookies } from 'better-auth/svelte-kit'
 
 interface TraktUser {
@@ -22,6 +22,10 @@ interface TraktUser {
 export const auth = betterAuth({
 	secret: PRIVATE_BETTER_AUTH_SECRET,
 	baseURL: PUBLIC_BETTER_AUTH_URL,
+	trustedOrigins: [
+		PUBLIC_BETTER_AUTH_URL,
+		...(process.env.NETLIFY_URL ? [process.env.NETLIFY_URL] : []),
+	],
 	// Stateless mode - no database required
 	// This will automatically enable JWT-based sessions in cookies
 	session: {
@@ -83,5 +87,8 @@ export const auth = betterAuth({
 			}
 		}),
 		sveltekitCookies(getRequestEvent),
+		oAuthProxy({
+			productionURL: PUBLIC_BETTER_AUTH_URL,
+		})
 	],
 })
