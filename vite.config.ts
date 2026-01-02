bcc4946a5f146e5e3e0ef899d60ba75e567f559a
LekoArts <lekoarts@gmail.com>
2026-01-02 19:41:50 +0100

    fix: Use other baseURL

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import { enhancedImages } from '@sveltejs/enhanced-img';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
+const DEPLOY_PRIME_URL = JSON.stringify(process.env.DEPLOY_PRIME_URL || '')
+
 export default defineConfig({
 	plugins: [enhancedImages(), sveltekit(), devtoolsJson()],
 	define: {
-		'__DEPLOY_PRIME_URL__': JSON.stringify(process.env.DEPLOY_PRIME_URL || ''),
+		'__DEPLOY_PRIME_URL__': process.env.CONTEXT === 'deploy-preview' ? DEPLOY_PRIME_URL : '""',
 	},
 	test: {
 		include: ['src/**/__tests__/*.ts'],
